679a0c732d9b5dd92e13480f8d69d0644715d9b3
Mikey Epps <55078570+meppps@users.noreply.github.com>
2019-11-07 10:02:28 -0800

    add

---
+++ VBA-Excel/data.vb
@@ -0,0 +1,9 @@
+Sub Calculate():
+    Dim openPrice As Double
+    Dim closePrice As Double
+    Dim yearlyChange As Double
+    
+    openPrice = Range("C2:C70926")
+    closePrice = Rnage("F2:F70926")
+    
+End Sub
