98e95f27cbad6d685be0211772da3aadc5c34c89
Mikey Epps <55078570+meppps@users.noreply.github.com>
2019-11-19 17:28:40 -0800

    Progress on stocks

--- stockTesting.vb
+++ stockTesting.vb
@@ -1,66 +1,4 @@
 
-' Cells(r,c)
-'------------------------------------
-Sub Stocks()
-
-    'Init Variables
-    Dim ticker As String
-    Dim currentTicker As String
-    Dim newTicker As String
-    Dim yearlyChange As Double
-    Dim percentChange As Double
-    Dim totalVolume As Long
-    Dim openPrice As Double
-    Dim closePrice As Double
-    Dim volume As Long
-    Dim outputRow As Integer
-    
-    ticker = ""
-    outputRow = 2
-    'Make Headers
-    Range("I1").Value = "Ticker"
-    Range("J1").Value = "Yearly Change"
-    Range("K1").Value = "Percent Change"
-    Range("L1").Value = "Total Stock Volume"
-
-    
-    openPrice = Cells(2, 3).Value
-    closePrice = Cells(2, 6).Value
-
-    Dim i As Integer
-    
-    For i = 2 To 70926
-
-    currentTicker = Cells(i, 1).Value
-    newTicker = Cells(i+1,1).Value
-    totalVolume = 0 
-    volume = cells(i,7).Value
-
-         If currentTicker <> newTicker Then
-            'Wrap it up
-
-            'ticker = currentTicker.value
-            'Add Vol
-            totalVolume = volume + totalVolume
-            outputrow = outputRow + 1
-            'Output()
-            Range("F",outputRow).Value = ticker
-        Else
-            'Just keep adding
-            totalVolume = volume + totalVolume
-        End if
-
-    Next i
-
-    sub output()
-        Range("F",outputRow).Value = ticker
-        'Range()
-        'Range()
-        'Range()
-    End Sub
-
-End Sub
-
 'Stocks V2------------------- WORKING !
 Sub Stocks()
 
@@ -87,21 +25,21 @@ Sub Stocks()
     
     openPrice = Cells(2, 3).Value
     closePrice = Cells(2, 6).Value
-    LastRow = ws.Cells(Rows.Count, 1).End(xlUp).Row
+    'LastRow = ws.Cells(Rows.Count, 1).End(xlUp).Row
 
     'Dim LastRow As Long
     'LastRow = ws.Cells(Rows.Count, 1).End(xlUp).Row
 
-    Dim i As Long
-    
-    For i = 2 To LastRow
-
-    currentTicker = Cells(i, 1).Value
+    Dim i As Longs
     newTicker = Cells(i + 1, 1).Value
     volume = Cells(i, 7).Value
 
 
          If currentTicker <> newTicker Then
+
+            If openPrice = 0 Then
+                openPrice = null 
+            End If
 
             closePrice = Cells(i,6).Value
 
@@ -135,7 +73,7 @@ Sub Stocks()
             totalVolume = volume + totalVolume
         End If
 
+       
     Next i
 
 End Sub
-
